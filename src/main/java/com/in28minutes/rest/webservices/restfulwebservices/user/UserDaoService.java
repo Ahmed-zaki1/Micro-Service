ac439be5be635873efe57625f8ba2ef415a84dff
Ahmed zaki <zaki40886@gmail.com>
2020-09-15 09:24:51 +0530

    Ahmed

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -2,20 +2,22 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDaoService {
+	
 	private static List<User> users = new ArrayList<>();
 
 	private static int usersCount = 3;
 
 	static {
 		users.add(new User(1, "Ahmed", new Date()));
-		users.add(new User(2, "zaki", new Date()));
-		users.add(new User(3, "siddiqui", new Date()));
+		users.add(new User(2, "Zaki", new Date()));
+		users.add(new User(3, "Siddiqui", new Date()));
 	}
 
 	public List<User> findAll() {
@@ -33,6 +35,18 @@ public class UserDaoService {
 	public User findOne(int id) {
 		for (User user : users) {
 			if (user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public User deleteById(int id) {
+		Iterator<User> iterator = users.iterator();
+		while (iterator.hasNext()) {
+			User user = iterator.next();
+			if (user.getId() == id) {
+				iterator.remove();
 				return user;
 			}
 		}
